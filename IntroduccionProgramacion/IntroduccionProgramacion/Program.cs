a0709a700e469574604c510b71b8a914f424db13
Rocio Amalfi <rocioamalfi22@gmail.com>
2020-01-25 13:54:53 -0300

    Clase 1

---
+++ IntroduccionProgramacion/IntroduccionProgramacion/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionProgramacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int var1 = 13;
+             const  int var2 = 3; //Constante no se puede cambiar el valor
+             int modulo = 0;
+
+             Console.WriteLine("Suma " + (var1 + var2));
+             Console.WriteLine("Multiplicacion " + (var1 * var2));
+             Console.WriteLine("Division" + (var1 / var2));
+             Console.WriteLine("Resta " + (var1 - var2));
+
+             //Modulo - devuelve el resto de la division
+             modulo = var1 % 5;
+             Console.WriteLine("Modulo de " + var1 +  " es : " + modulo);
+
+             //Ingreso de datos por consola
+
+             int numero1 = Int32.Parse(Console.ReadLine());
+             Console.WriteLine("Usted ingreso : " + numero1);*/
+
+            
+            //Ejercicio 1
+            int n1 = 5;
+            int n2 = 10;
+            int n3 = 20;
+            int promedio = 0;
+            int modulo = 0;
+            
+            Console.WriteLine("Suma: " + (n1 +n2));
+            Console.WriteLine("Resta: " + (n3 - n1));
+            Console.WriteLine("Multiplicacion: " + (n1 * n3));
+            Console.WriteLine("Division: " + (n3 + n2));
+
+            // Ejercicio 2
+            Console.WriteLine();
+            Console.WriteLine("Ingrese el primer numero : ");
+            n1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero : ");
+            n2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tercer numero : ");
+            n3 = Int32.Parse(Console.ReadLine());
+
+            promedio = (n1 + n2 + n3) / 3;
+            modulo = n2 % n1;
+            Console.WriteLine("El Total es: " + (n1 +n2 + n3));
+            Console.WriteLine("El promedio es: " + promedio);
+            Console.WriteLine("El resto entre el primer y segundo numero es : " + modulo);
+
+          
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ IntroduccionProgramacion/IntroduccionProgramacion/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("IntroduccionProgramacion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntroduccionProgramacion")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("54e40a7e-55f9-46d2-83b4-587c5befce7c")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
